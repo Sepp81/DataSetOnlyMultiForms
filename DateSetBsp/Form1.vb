2165f112b3f71f311d715fdb9af71b4d4cbfc1a3
Sepp <sepp_81@byom.de>
2014-06-24 15:17:56 +0200

    verbessert durch EDR

--- DateSetBsp/Form1.vb
+++ DateSetBsp/Form1.vb
@@ -4,58 +4,35 @@
 Option Strict On
 Option Explicit On
 
+
 Imports System.IO
-Imports System.Reflection
-Imports System.Math
 
 Public Class Form1
 
     Private _kundendaten As New FileInfo("Kundendaten.xml")
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim Bildschirm As Screen = Screen.PrimaryScreen
-        Location = New Point(Bildschirm.Bounds.Left + (Bildschirm.Bounds.Width - Width) \ 2, Bildschirm.Bounds.Top)
-
+    Sub New()
+        InitializeComponent()
+        Me.AlignOnTop()
         DataSet1.DataFile(_kundendaten.FullName).Register(Me, True).Fill()
-
     End Sub
 
-    '### MenuStrip ###
-    Private Sub KundenEingebenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KundenEingebenToolStripMenuItem.Click
-        Dim KundenBearbeiten As New frmKundenBearbeiten
-        If KundenBearbeiten Is Nothing OrElse frmKundenBearbeiten.IsDisposed Then
-            KundenBearbeiten = New frmKundenBearbeiten
-        End If
-        If Not KundenBearbeiten.Visible Then
-            KundenBearbeiten.Show(Me)
-        End If
-        If KundenBearbeiten.WindowState = FormWindowState.Minimized Then
-            KundenBearbeiten.WindowState = FormWindowState.Normal
-        End If
+    Private Sub KundenEingebenToolStripMenuItem_Click(sender As Object, e As  _
+      EventArgs) Handles KundenEingebenToolStripMenuItem.Click
+        frmKundenBearbeiten.Show(Me)
     End Sub
 
-    Private Sub BeendenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BeendenToolStripMenuItem.Click
-        Me.Close()
+    Private Sub cmdFindPhoneNr_Click(sender As Object, e As EventArgs) Handles _
+      cmdKundenFinden.Click
+        Dim Eingabe = 0
+        If Integer.TryParse(txtFrmMainKundennummer.Text, Eingabe) Then
+            Dim rwKunde = DataSet1.Kunde.FindByKundennummer(Eingabe)
+            If rwKunde.NotNull Then
+                lblFrmMainKundenTeleNr.Text = rwKunde.Telefon
+                Return
+            End If
+        End If
+        MessageBox.Show("Keine gültige Zahl eingegeben oder Kein Kunden gefunden")
     End Sub
 
-    '### Hauptfunktionalität ###
-    Private Sub cmdFindPhoneNr_Click(sender As Object, e As EventArgs) Handles cmdKundenFinden.Click
-        Dim FindeDenKunden As New frmKundenBearbeiten
-        Dim Eingabe As Integer
-        Try
-            Eingabe = Convert.ToInt32(txtFrmMainKundennummer.Text)
-        Catch ex As Exception
-            Eingabe = Nothing
-        End Try
-
-        If Eingabe = Nothing Then
-            MessageBox.Show("Keine gültige Zahl eingegeben")
-        End If
-        Dim gefundenKunden = FindeDenKunden.KundenFinden(Eingabe)
-        If gefundenKunden Is Nothing Then
-            MessageBox.Show("Kein Kunden gefunden")
-        Else
-            lblFrmMainKundenTeleNr.Text = gefundenKunden.KundeRow.Name
-        End If
-    End Sub
 End Class
--- DateSetBsp/frmKundenBearbeiten.vb
+++ DateSetBsp/frmKundenBearbeiten.vb
@@ -7,21 +7,10 @@ Imports System.IO
 
 Public Class frmKundenBearbeiten
 
-    Private Sub frmKundenBearbeiten_Load(sender As Object, e As EventArgs) _
-      Handles MyBase.Load
+    Sub New()
+        InitializeComponent()
         Me.AlignOnTop()
-        DataSet1.Register(Me, True)
-    End Sub
-
-    '### MenuStrip ###
-    Private Sub KundeTelToolStripMenuItem_Click(sender As Object, e As  _
-      EventArgs) Handles KundeTelToolStripMenuItem.Click
-        Dim s = Microsoft.VisualBasic.InputBox("Kundennr eingeben")
-        If s = "" Then Return
-        Dim ID = Integer.Parse(s)
-        KundeBindingSource.MoveTo(ID)
-        Dim gefundenerKunde = DataSet1.Kunde.FindByKundennummer(ID)
-        MessageBox.Show(gefundenerKunde.Telefon)
+        DataSet1.Register(Me, False)
     End Sub
 
     Private Sub SpeichernToolStripMenuItem_Click(sender As Object, e As  _
@@ -29,19 +18,12 @@ Public Class frmKundenBearbeiten
         DataSet1.Save(Me)
     End Sub
 
-    Private Sub BeendenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BeendenToolStripMenuItem.Click
-        DataSet1.Save(Me)
-        Me.Close()
+    Private Sub KundeTelToolStripMenuItem_Click(sender As Object, e As  _
+      EventArgs) Handles KundeTelToolStripMenuItem.Click
+        Dim s = Microsoft.VisualBasic.InputBox("Kundennr eingeben")
+        If s = "" Then Return
+        Dim ID = Integer.Parse(s)
+        KundeBindingSource.MoveTo(ID)
     End Sub
 
-    '### Funktion um den Kunden Datensatz zu finden ###
-    Public Function KundenFinden(ByRef Kundennummer As Integer) As DetailsBestellungRow
-        Dim gefundenerKunde = DataSet1.Kunde.FindByKundennummer(Kundennummer)
-        Dim gefundenerKundenDatensatz = GetKundenChildRow(gefundenerKunde)
-        Return gefundenerKundenDatensatz
-    End Function
-
-    Private Function GetKundenChildRow(ByVal rwKunde As KundeRow) As DetailsBestellungRow
-        Return (From rwDetails In DataSet1.Kunde.GetDetailsBestellungRows)
-    End Function
 End Class
